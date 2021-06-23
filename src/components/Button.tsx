ec3581d81f170764db3f4920b0141633a17e5d2d
unknown <otavio.paiva93@yahoo.com.br>
2021-06-23 17:57:27 -0300

    Paginas finalizadas

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import { ButtonHTMLAttributes } from 'react';
+
+import '../styles/button.scss';
+
+type Buttonprops = ButtonHTMLAttributes<HTMLButtonElement>
+
+export function Button(props: Buttonprops){
+    return (
+        <button className="button" {...props} />
+    )
+}
